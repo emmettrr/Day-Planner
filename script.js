f34ab9c80bc773666fcf62d6f41d2f4519715db1
Emmett Rushin <emmettrushin@gmail.com>
2021-04-26 12:26:40 -0400

    added button function for local storage

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let nowTime = moment().format("dddd, MMMM Do YYYY");
 let hourTime = moment().format("h");
 let current24 = moment().format("HH");
 
+const hr9 = $("#hour1");
+const hr10 = $("#hour2"); 
+const hr11 = $("#hour3");
+const hr12 = $("#hour4"); 
+const hr1 = $("#hour5");
+const hr2 = $("#hour6"); 
+const hr3 = $("#hour7"); 
+const hr4 = $("#hour8"); 
+const hr5 = $("#hour9");  
+
+let btn1 = $("#btn1");
+
+$("#btn1").click(function() {
+    localStorage.setItem("#text3").text()
+})
