7d454f8f9b984992146ec13c8394c3433c3dd120
Emmett Rushin <emmettrushin@gmail.com>
2021-04-27 11:22:00 -0400

    working on js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-let nowTime = moment().format("dddd, MMMM Do YYYY");
+let nowTime = moment().format("dddd, MMMM YYYY");
 let hourTime = moment().format("h");
-let current24 = moment().format("HH");
 
 const hr9 = $("#hour1");
 const hr10 = $("#hour2"); 
 const hr11 = $("#hour3");
 const hr12 = $("#hour4"); 
-const hr1 = $("#hour5");
-const hr2 = $("#hour6"); 
-const hr3 = $("#hour7"); 
-const hr4 = $("#hour8"); 
-const hr5 = $("#hour9");  
+const hr13 = $("#hour5");
+const hr14 = $("#hour6"); 
+const hr15 = $("#hour7"); 
+const hr16 = $("#hour8"); 
+const hr17 = $("#hour9");  
 
-let btn1 = $("#btn1");
+let btn = $("#btn");
 
 $("#btn1").click(function() {
-    localStorage.setItem("#text3").text()
+    localStorage.setItem("#text3").text();
 })
+
+$("#currentDay").text(moment().format("dddd, MMMM YYYY"))
+
+// $(function()
+// $(#btn).click(function () {
+//     $()
+// }
