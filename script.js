96c4810e953c1255f5d8b02b6e1bc5d11fd291b1
Emmett Rushin <emmettrushin@gmail.com>
2021-04-27 12:42:44 -0400

    removed variables and fixed local storage

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
 let nowTime = moment().format("dddd, MMMM YYYY");
 let currentHour = parseInt(moment().format("H"));
 const rows = document.getElementsByClassName('row')
-const btn1 = $("#btn1");
-const btn2 = $("#btn2");
-const btn3 = $("#btn3");
-const btn4 = $("#btn4");
-const btn5 = $("#btn5");
-const btn6 = $("#btn6");
-const btn7 = $("#btn7");
-const btn8 = $("#btn8");
-const btn9 = $("#btn9");
-const text1 = $("#text1")
-const text2 = $("#text2")
-const text3 = $("#text3")
-const text4 = $("#text4")
-const text5 = $("#text5")
-const text6 = $("#text6")
-const text7 = $("#text7")
-const text8 = $("#text8")
-const text9 = $("#text9")
+const saveBtn = $('.row').children('.btn');
 
-$("#btn1").click(function() {
-    localStorage.setItem("")
+saveBtn.click(function(event) {
+    event.preventDefault();
+    var textInput = $(this.parentNode).children('.content').attr('id');
+    var inputToStorage = $(this.parentNode).children('.content').val();
+    localStorage.setItem(textInput, inputToStorage)
 })
 
 $("#currentDay").text(moment().format("dddd, MMMM YYYY"))
