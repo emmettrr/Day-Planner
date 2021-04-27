8a4acc111a2420f8d080c00621b8db40c0eb7463
Emmett Rushin <emmettrushin@gmail.com>
2021-04-27 11:33:32 -0400

    added function to compare hours and change color depending on comparison

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let nowTime = moment().format("dddd, MMMM YYYY");
-let hourTime = moment().format("h");
-
+let currentHour = moment().format("h");
 const hr9 = $("#hour1");
 const hr10 = $("#hour2"); 
 const hr11 = $("#hour3");
@@ -9,7 +8,8 @@ const hr13 = $("#hour5");
 const hr14 = $("#hour6"); 
 const hr15 = $("#hour7"); 
 const hr16 = $("#hour8"); 
-const hr17 = $("#hour9");  
+const hr17 = $("#hour9");
+const rows = document.getElementsByClassName('row')
 
 let btn = $("#btn");
 
@@ -19,7 +19,22 @@ $("#btn1").click(function() {
 
 $("#currentDay").text(moment().format("dddd, MMMM YYYY"))
 
-// $(function()
-// $(#btn).click(function () {
-//     $()
-// }
+Array.from('row').forEach(row => {
+    let rowIDString= row.id,
+    rowHour;
+if (rowIDString) {
+    rowHour = parseInt(rowIDString);
+}
+if (rowHour) {
+    if (currentHour === rowHour) {
+        setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+        setColor(row, "green");
+
+    } else if ((currentHour < rowHour) && (currentHour > rowHour + 6)) {
+        setColor(row, "lightgrey");
+    } else {
+        setColor(row,"white");
+    }
+}
+});
